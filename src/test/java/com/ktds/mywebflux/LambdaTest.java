0a27b31dd26955df542518427c08dfb37c0eb96f
myvega2k <vega2k@kakao.com>
2023-06-26 13:57:44 +0900

    lambda update

--- src/test/java/com/ktds/mywebflux/LambdaTest.java
+++ src/test/java/com/ktds/mywebflux/LambdaTest.java
@@ -6,6 +6,15 @@ public class LambdaTest {
     @Test
     void runnable() {
         //1. Anonymous Inner Class 형태로  Runnable을 구현하기
-
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonymous Inner Class");
+            }
+        });
+        t1.start();
+        //2. Lambda Expression
+        Thread t2 = new Thread(() -> System.out.println("Lambda Expression"));
+        t2.start();
     }
 }
