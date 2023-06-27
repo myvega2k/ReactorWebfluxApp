fa3fa1d20bd357e213dfb9feb27657693cb55e65
myvega2k <vega2k@kakao.com>
2023-06-27 10:02:13 +0900

    controller add

---
+++ src/main/java/com/ktds/mywebflux/controller/R2CustomerController.java
@@ -0,0 +1,19 @@
+package com.ktds.mywebflux.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@RestController
+public class R2CustomerController {
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "Flux - " + LocalTime.now().toString());
+    }
+
+}
