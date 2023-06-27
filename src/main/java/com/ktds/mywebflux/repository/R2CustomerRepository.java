f20e7cd7f9117c3134e931a06ec5be96dd7c9234
myvega2k <vega2k@kakao.com>
2023-06-27 10:40:19 +0900

    repository add

---
+++ src/main/java/com/ktds/mywebflux/repository/R2CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.ktds.mywebflux.repository;
+
+import com.ktds.mywebflux.entity.Customer;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface R2CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
+    @Query("SELECT * FROM CUSTOMER WHERE last_name = :lastName")
+    Flux<Customer> findByLastName(String lastName);
+}
