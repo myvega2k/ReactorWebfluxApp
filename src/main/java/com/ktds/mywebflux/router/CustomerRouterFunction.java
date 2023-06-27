1c387e523dd44da92e20a974522dc11ec2f769bf
myvega2k <vega2k@kakao.com>
2023-06-27 16:59:43 +0900

    customerhandler savecustomer add

--- src/main/java/com/ktds/mywebflux/router/CustomerRouterFunction.java
+++ src/main/java/com/ktds/mywebflux/router/CustomerRouterFunction.java
@@ -2,13 +2,20 @@ package com.ktds.mywebflux.router;
 
 import com.ktds.mywebflux.entity.Customer;
 import com.ktds.mywebflux.handler.CustomerHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.*;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
@@ -18,6 +25,34 @@ public class CustomerRouterFunction {
     public WebProperties.Resources resources() {
         return new WebProperties.Resources();
     }
+
+    @RouterOperations({
+            @RouterOperation(path = "/router/r2customers", method = RequestMethod.GET,
+                    beanClass = CustomerHandler.class, beanMethod = "getCustomers"),
+            @RouterOperation(path = "/router/r2customers/{id}", method = RequestMethod.GET,
+                    beanClass = CustomerHandler.class, beanMethod = "getCustomer",
+                    operation = @Operation(operationId = "getCustomer",
+                            parameters = {@Parameter(in = ParameterIn.PATH, name = "id")})
+            ),
+            @RouterOperation(path = "/router/r2customers", method = RequestMethod.POST,
+                    beanClass = CustomerHandler.class, beanMethod = "saveCustomer",
+                    operation = @Operation(operationId = "saveCustomer",
+                            requestBody = @RequestBody(content =
+                            @Content(schema = @Schema(implementation = Customer.class))))
+            ),
+            @RouterOperation(path = "/router/r2customers/{id}", method = RequestMethod.PUT,
+                    beanClass = CustomerHandler.class, beanMethod = "updateCustomer",
+                    operation = @Operation(operationId = "updateCustomer",
+                            parameters = {@Parameter(in = ParameterIn.PATH, name = "id")},
+                            requestBody = @RequestBody(content =
+                            @Content(schema = @Schema(implementation = Customer.class))))
+            ),
+            @RouterOperation(path = "/router/r2customers/{id}", method = RequestMethod.DELETE,
+                    beanClass = CustomerHandler.class, beanMethod = "deleteCustomer",
+                    operation = @Operation(operationId = "deleteCustomer",
+                            parameters = {@Parameter(in = ParameterIn.PATH, name = "id")})
+            )
+    })
 
     @Bean
     public RouterFunction<ServerResponse> routerFunction(CustomerHandler customerHandler) {
